594295194fc0660e96826edc32dab526b42217e3
yoke <1013812851@qq.com>
2019-12-24 20:45:57 +0800

    chapter04: mongodb

--- chapter04/src/main/kotlin/io/kang/example/repository/StudentRepository.kt
+++ chapter04/src/main/kotlin/io/kang/example/repository/StudentRepository.kt
@@ -3,6 +3,7 @@ package io.kang.example.repository
 import io.kang.example.entity.Student
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.DeleteQuery
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 
@@ -11,8 +12,9 @@ interface StudentRepository: MongoRepository<Student, Long> {
 
     fun findByNameRegexAndAgeGreaterThanEqual(name: String, age: Int, pageable: Pageable): Page<Student>
 
-    fun deleteByAgeGreaterThanAndAgeLessThan(age1: Int, age2: Int): Int
+    @DeleteQuery(value = "{\"age\":{\"\$gte\":?0,\"\$lte\":?1}}")
+    fun deleteByAgeIn(age1: Int, age2: Int)
 
-    @Query(value = "{\"name\":{\"\$regex\":?0},\"age\":{\"\$gte\":?1}}", fields = "{\"personId\":1,\"age\":1}")
+    @Query(value = "{\"name\":{\"\$regex\":?0},\"age\":{\"\$gte\":?1}}")
     fun findByAgeIndividual(name: String ,age: Int, pageable: Pageable): Page<Student>
 }
--- chapter04/src/test/kotlin/io/kang/example/repository/StudentRepositoryTest.kt
+++ chapter04/src/test/kotlin/io/kang/example/repository/StudentRepositoryTest.kt
@@ -1,6 +1,7 @@
 package io.kang.example.repository
 
 import io.kang.example.entity.Student
+import org.junit.Assert
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
@@ -25,10 +27,38 @@ class StudentRepositoryTest {
         student.age = 22
         student.name = "张三"
 
+        studentRepository.deleteAll()
+
         for(i in 0..15) {
-            student.age = student.age - i
-            student.personId = student.personId + i
+            student.age = 22 - i
+            student.personId = 20180101L + i
             studentRepository.save(student)
         }
+    }
+
+    @Test
+    @Order(2)
+    fun testFindByNameAndAge() {
+        val pageable = PageRequest.of(0, 5)
+
+        val studentPage = studentRepository.findByNameRegexAndAgeGreaterThanEqual("张三", 20, pageable)
+
+        Assert.assertEquals(3, studentPage.content.size)
+    }
+
+    @Test
+    @Order(3)
+    fun testFindByNameAndAge1() {
+        val pageable = PageRequest.of(0, 5)
+        val studentPage = studentRepository.findByAgeIndividual("张三", 10, pageable)
+        Assert.assertEquals(5, studentPage.content.size)
+    }
+
+    @Test
+    @Order(4)
+    fun testDeleteByAgeGreaterThanAndAgeLessThan() {
+        studentRepository.deleteByAgeIn(20, 22)
+        val students = studentRepository.findAll()
+        Assert.assertEquals(13, students.size)
     }
 }
