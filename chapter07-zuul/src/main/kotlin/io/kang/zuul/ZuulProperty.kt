f91dae96fee0eab34d6172631868df8d9455ad37
yoke <1013812851@qq.com>
2020-02-15 11:04:24 +0800

    chapter07: springcloud zuul config

--- chapter07-zuul/src/main/kotlin/io/kang/zuul/ZuulProperty.kt
+++ chapter07-zuul/src/main/kotlin/io/kang/zuul/ZuulProperty.kt
@@ -3,12 +3,16 @@ package io.kang.zuul
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
-@ConfigurationProperties("zuul")
-@RefreshScope
 class ZuulProperty {
+    @Bean
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    @Primary
     fun zuulProperties(): ZuulProperties {
         return ZuulProperties()
     }
