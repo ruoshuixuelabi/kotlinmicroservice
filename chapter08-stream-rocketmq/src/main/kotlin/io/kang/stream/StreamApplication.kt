9ba2f3fb36ab27d44909d5b7c87d0d57ceb3f391
yoke <1013812851@qq.com>
2020-02-29 20:12:55 +0800

    chapter08: springcloud stream rocketmq，consumer group，partition

--- chapter08-stream-rocketmq/src/main/kotlin/io/kang/stream/StreamApplication.kt
+++ chapter08-stream-rocketmq/src/main/kotlin/io/kang/stream/StreamApplication.kt
@@ -3,11 +3,9 @@ package io.kang.stream
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.messaging.Sink
-import org.springframework.cloud.stream.messaging.Source
 
 @SpringBootApplication
-@EnableBinding(Source::class, Sink::class)
+@EnableBinding(MySource::class, MySink::class)
 class StreamApplication
 
 fun main(args: Array<String>) {
