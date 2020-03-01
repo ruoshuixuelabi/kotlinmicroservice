80d588b6cc7a93e7e854e4de02c436db65b3d910
yoke <1013812851@qq.com>
2020-03-01 21:20:03 +0800

    chapter08: springcloud oss images

--- chapter08-oss/src/main/kotlin/io/kang/oss/OssController.kt
+++ chapter08-oss/src/main/kotlin/io/kang/oss/OssController.kt
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.apache.commons.codec.CharEncoding
 import org.springframework.util.StreamUtils
 import java.nio.charset.Charset
-
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @RestController
 class OssController {
@@ -27,37 +28,31 @@ class OssController {
     @GetMapping("/upload")
     fun upload(): String {
         try {
-            ossClient.putObject("kcglobal", "test/oss-test.json", this.javaClass.classLoader.getResourceAsStream("oss-test.json"))
+            val stream = this.javaClass.classLoader.getResourceAsStream("oss-test.json")
+            ossClient.putObject("kcglobal", "test/oss-test.json", stream)
         } catch (e: Exception) {
-            e.printStackTrace()
             return "upload fail: " + e.message
         }
-
         return "upload success"
     }
 
     @GetMapping("/file-resource")
     fun fileResource(): String {
-        try {
-            return "get file resource success. content: " + StreamUtils.copyToString(
-                    remoteFile.inputStream, Charset.forName(CharEncoding.UTF_8))
+        return try {
+            "get file resource success. content: " +
+                    StreamUtils.copyToString(remoteFile.inputStream, Charset.forName(CharEncoding.UTF_8))
         } catch (e: Exception) {
-            e.printStackTrace()
-            return "get resource fail: " + e.message
+            "get resource fail: " + e.message
         }
-
     }
 
     @GetMapping("/download")
     fun download(): String {
-        try {
-            val ossObject = ossClient.getObject("kcglobal",
-                    "test/oss-test.json")
-            return "download success, content: " + IOUtils
-                    .readStreamAsString(ossObject.objectContent, CharEncoding.UTF_8)
+        return try {
+            val ossObject = ossClient.getObject("kcglobal", "test/oss-test.json")
+            "download success, content: " + IOUtils.readStreamAsString(ossObject.objectContent, CharEncoding.UTF_8)
         } catch (e: Exception) {
-            e.printStackTrace()
-            return "download fail: " + e.message
+            "download fail: " + e.message
         }
 
     }
@@ -66,9 +61,12 @@ class OssController {
     fun uploadWithOutputStream(): String {
         try {
             (this.remoteFile as WritableResource)
-                    .outputStream.use { outputStream -> localFile.inputStream.use { inputStream -> StreamUtils.copy(inputStream, outputStream) } }
+                    .outputStream
+                    .use { outputStream ->
+                        localFile.inputStream.use { inputStream -> StreamUtils.copy(inputStream, outputStream)
+                        }
+                    }
         } catch (ex: Exception) {
-            ex.printStackTrace()
             return "upload with outputStream failed"
         }
 
